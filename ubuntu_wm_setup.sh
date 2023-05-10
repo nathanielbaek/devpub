9e260b0314ffdc04ec360a2cff0178dde79d284a
nathanielbaek <45681816+nathanielbaek@users.noreply.github.com>
2023-05-10 16:24:12 +0900

    Update ubuntu_wm_setup.sh

--- ubuntu_wm_setup.sh
+++ ubuntu_wm_setup.sh
@@ -85,15 +85,15 @@ if [ "$(id -u)" != "0" ]; then
               systemctl disable filebeat
 
           # Configure motd
-              echo '---------------------------------------------------------------' > /etc/motd.tail
-              echo '               Prohibition of non-authorised use               ' >> /etc/motd.tail
-              echo '---------------------------------------------------------------' >> /etc/motd.tail
-              echo 'System Information:'                                             >> /etc/motd.tail
-              echo 'Hostname: $(hostname)'                                           >> /etc/motd.tail
-              echo 'Uptime: $(uptime | awk '{print $3,$4}' | sed 's/,//')'           >> /etc/motd.tail
-              echo 'Memory Usage: $(free | awk '/Mem/{printf("%.2f%"), $3/$2*100}') >> /etc/motd.tail
-              echo 'Disk Usage: $(df -h / | awk '/\//{print $(NF-1)}')'              >> /etc/motd.tail
-              echo '---------------------------------------------------------------' >> /etc/motd.tail
+              echo '---------------------------------------------------------------' > /etc/update-motd.d/99-message
+              echo '               Prohibition of non-authorised use               ' >> /etc/update-motd.d/99-message
+              echo '---------------------------------------------------------------' >> /etc/update-motd.d/99-message
+              echo 'System Information:'                                             >> /etc/update-motd.d/99-message
+              echo 'Hostname: $(hostname)'                                           >> /etc/update-motd.d/99-message
+              echo 'Uptime: $(uptime | awk '{print $3,$4}' | sed 's/,//')'           >> /etc/update-motd.d/99-message
+              echo 'Memory Usage: $(free | awk '/Mem/{printf("%.2f%"), $3/$2*100}')' >> /etc/update-motd.d/99-message
+              echo 'Disk Usage: $(df -h / | awk '/\//{print $(NF-1)}')'              >> /etc/update-motd.d/99-message
+              echo '---------------------------------------------------------------' >> /etc/update-motd.d/99-message
                 else
                   echo "스크립트 실행이 중지되었습니다. 다시 실행해주세요"
                     exit 1
